56e0cc93f9ea8f767dbffc4158ff31a1ebcef5e1
Jun <jkubo20@stuy.edu>
2020-12-22 21:59:12 -0500

    all finished

--- Driver.java
+++ Driver.java
@@ -1,4 +1,6 @@
 import java.util.Arrays;
+import java.util.Random;
+
 public class Driver{
   public static void main(String[]args){
     Sorts sorter = new Sorts();
@@ -10,26 +12,42 @@ public class Driver{
 
 
       int[] randData = new int[Integer.parseInt(args[0])];
+      Random rng = new Random(1);
 
       //overhead: 1 random array generation.
       if(args.length < 3 || args[2].equals("random")){
-	      System.out.println("REPLACE THIS LINE WITH ARRAY VALUE GENERATION!");
+        rng = new Random(100);//seed of 100 is stored.
+        for(int i =  0; i < Integer.parseInt(args[0]); i++){
+          randData[i] = rng.nextInt() % 1000;
+        }
       }else if(args[2].equals("equal")){
-	      System.out.println("REPLACE THIS LINE WITH ARRAY VALUE GENERATION!");
+        // array of all same ?
+        rng = new Random(100);
+        int same = rng.nextInt() % 1000;
+        for (int i = 0; i < Integer.parseInt(args[0]); i++) {
+          randData[i] = same;
+        }
       }else if(args[2].equals("sorted")){
-	      System.out.println("REPLACE THIS LINE WITH ARRAY VALUE GENERATION!");
-      }else if(args[2].equals("reversed")){
-	      System.out.println("REPLACE THIS LINE WITH ARRAY VALUE GENERATION!");
+	      for (int i = 0; i < Integer.parseInt(args[0]); i++) {
+          randData[i] = i;
+        }
+      } else if(args[2].equals("reversed")){
+	      for (int i = 0; i < Integer.parseInt(args[0]); i++) {
+          randData[i] = Integer.parseInt(args[0]) - i;
+        }
       }
 
       if(args[1].equals("bubble")){
         sorter.bubbleSort(randData);
+        //sorter.printArray(randData);
       }
       else if(args[1].equals("selection")){
         sorter.selectionSort(randData);
+        //sorter.printArray(randData);
       }
       else if(args[1].equals("insertion")){
         sorter.insertionSort(randData);
+        //sorter.printArray(randData);
       }
 
       //TEST MODE! This lets you see if your algorithm fails
